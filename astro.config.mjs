3f742cab9825c8c88e883ff5553c53db1f7b32e7
Tim Schletter <schletterdesign@gmail.com>
2026-02-05 12:28:45 -0500

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  site: 'https://timprodev.github.io',
-  base: '/design-website',
+  // site: 'https://timprodev.github.io',
+  // base: '/design-website',
 });
